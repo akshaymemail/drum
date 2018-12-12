85191ce1bf385f42dec70448ac62ab92d3700444
Akshay Singh <akshaymemail@gmail.com>
2018-12-12 08:03:43 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -16,25 +16,25 @@ document.addEventListener('keypress', function () {
 function playSound(key) {
     switch (key) {
         case 'w':
-            new Audio('sounds/tom-1.mp3').play();
+            new Audio('tom-1.mp3').play();
             break
         case 'a':
-            new Audio('sounds/tom-2.mp3').play();
+            new Audio('tom-2.mp3').play();
             break;
         case 's':
-            new Audio('sounds/tom-3.mp3').play();
+            new Audio('tom-3.mp3').play();
             break;
         case 'd':
-            new Audio('sounds/tom-4.mp3').play();
+            new Audio('tom-4.mp3').play();
             break;
         case 'j':
-            new Audio('sounds/snare.mp3').play();
+            new Audio('snare.mp3').play();
             break;
         case 'k':
-            new Audio('sounds/crash.mp3').play();
+            new Audio('crash.mp3').play();
             break;
         case 'l':
-            new Audio('sounds/kick-bass.mp3').play();
+            new Audio('kick-bass.mp3').play();
             break;
         default:
             console.log("Wrong Key Pressed...");
